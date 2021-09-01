aa8c138aff910e724f76cdffd9c0dc1e905a18a3
allyite <89873811+allyite@users.noreply.github.com>
2021-09-02 01:34:14 +0530

    Create api.go

---
+++ api/api.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+//	"handlers"
+
+	"github.com/labstack/echo"
+)
+
+func MainGroup(e *echo.Echo) {
+	// Route / to handler function
+  /*
+	e.GET("/health-check", handlers.HealthCheck)
+
+	e.GET("/cats/:data", handlers.GetCats)
+	e.POST("/cats", handlers.AddCat)
+  */
+  e.GET("/dogs", func(c echo.Context) error {       
+	return c.String(http.StatusOK, "Hello, Dogs!\n")  
+	})
+
+}
+/*
+func AdminGroup(g *echo.Group) {
+	g.GET("/main", handlers.MainAdmin)
+}
+*/
