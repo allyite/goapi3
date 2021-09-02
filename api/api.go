0fffe5dddf2b4497544e22658d8ad4866173de9d
allyite <89873811+allyite@users.noreply.github.com>
2021-09-02 17:41:37 +0530

    Update api.go

--- api/api.go
+++ api/api.go
@@ -2,11 +2,13 @@ package api
 
 import (
 	"github.com/allyite/goapi3/handlers"
+	"github.com/allyite/goapi3/helper"
 	"net/http"
 	"github.com/labstack/echo"
 )
 
 func MainGroup(e *echo.Echo) {
+	var collection = helper.ConnectDB()
 	// Route / to handler function
   /*
 	e.GET("/health-check", handlers.HealthCheck)
@@ -14,12 +16,20 @@ func MainGroup(e *echo.Echo) {
 	e.GET("/cats/:data", handlers.GetCats)
 	e.POST("/cats", handlers.AddCat)
   */
-	e.GET("/cats/:data", handlers.GetStats)
+	
+	handler1 := func(c echo.Context) error {
+        	return handlers.GetStats(c, collection)
+        }
+
+	e.GET("/cats/:data", handler1)
   	e.GET("/dogs", func(c echo.Context) error {       
 	return c.String(http.StatusOK, "Hello, Dogs!\n")  
 	})
 
 }
+
+
+
 /*
 func AdminGroup(g *echo.Group) {
 	g.GET("/main", handlers.MainAdmin)
