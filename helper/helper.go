3715495642ea5d4bcb6bc4a497386b80a1f10838
allyite <89873811+allyite@users.noreply.github.com>
2021-09-02 16:48:53 +0530

    Update helper.go

--- helper/helper.go
+++ helper/helper.go
@@ -58,7 +58,7 @@ func GetError(err error, w http.ResponseWriter) {
 
 // Configuration model
 type Configuration struct {
-	Port             string
+	////Port             string
 	ConnectionString string
 }
 
@@ -71,7 +71,7 @@ func GetConfiguration() Configuration {
 	}
 
 	configuration := Configuration{
-		os.Getenv("PORT"),
+		////os.Getenv("PORT"),
 		os.Getenv("CONNECTION_STRING"),
 	}
 
