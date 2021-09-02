e8dc63a667fec00f9f874931a81c440b42499eda
allyite <89873811+allyite@users.noreply.github.com>
2021-09-02 12:53:28 +0530

    Update handler_stats.go

--- handlers/handler_stats.go
+++ handlers/handler_stats.go
@@ -1,1 +1,31 @@
+package handlers
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/allyite/goapi3/models"
+
+	"github.com/labstack/echo"
+)
+
+//http://localhost:8000/cats/json?name=arnold&type=fluffy
+func GetStats(c echo.Context) error {
+	sName := c.QueryParam("name")
+	dataType := c.Param("data")
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("your state name is : %s\n", sName))
+	} else if dataType == "json" {
+		state := models.State{
+			Name: sName,
+		}
+		return c.JSON(http.StatusOK, state)
+	} else {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "Please specify the data type as Sting or JSON",
+		})
+	}
+
+}
