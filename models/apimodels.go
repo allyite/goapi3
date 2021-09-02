a3280789af989ac2df86c7ecca630a61efd19e8a
allyite <89873811+allyite@users.noreply.github.com>
2021-09-02 16:09:09 +0530

    Update apimodels.go

--- models/apimodels.go
+++ models/apimodels.go
@@ -1,5 +1,7 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 //Response
 type HealthCheckResponse struct {
 	Message string `json:"message"`
@@ -13,4 +15,29 @@ type Cat struct {
 
 type State struct {
 	Name string `json:"name"`
+}
+
+/*
+type StateResp struct {
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Isbn   string             `json:"isbn,omitempty" bson:"isbn,omitempty"`
+	Title  string             `json:"title" bson:"title,omitempty"`
+	Author *Author            `json:"author" bson:"author,omitempty"`
+}
+*/
+
+type StateResp struct {
+ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+active string `json:"active" bson:"active"`
+confirmed string `json:"confirmed" bson:"confirmed"`
+deaths string `json:"deaths" bson:"deaths"`
+deltaconfirmed string `json:"deltaconfirmed" bson:"deltaconfirmed"`
+deltadeaths string `json:"deltadeaths" bson:"deltadeaths"`
+deltarecovered string `json:"deltarecovered" bson:"deltarecovered"`
+lastupdatedtime string `json:"lastupdatedtime" bson:"lastupdatedtime"`
+migratedother string `json:"migratedother" bson:"migratedother"`
+recovered string `json:"recovered" bson:"recovered"`
+state string `json:"state" bson:"state"`
+statecode string `json:"statecode" bson:"statecode"`
+statenotes string `json:"statenotes" bson:"statenotes"`
 }
