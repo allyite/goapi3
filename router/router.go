aa9fe27a15315626a0ebb15c8258c17632fb23eb
allyite <89873811+allyite@users.noreply.github.com>
2021-09-03 17:12:02 +0530

    Update router.go

--- router/router.go
+++ router/router.go
@@ -2,26 +2,48 @@ package router
 
 import (
 	"github.com/allyite/goapi3/api"
+	"github.com/allyite/goapi3/handlers"
+//	"github.com/allyite/goapi3/helper"
+	
+	"net/http"
 //	"api/middlewares"
 
 	"github.com/labstack/echo"
 )
 
-func New() *echo.Echo {
+func New(mClient *mongo.Client) *echo.Echo {
+	
 	// create a new echo instance
 	e := echo.New()
 
-	//create groups
-//	adminGroup := e.Group("/admin")
+	handler1 := func(c echo.Context) error {
+        	return handlers.GetStats(c, mClient)
+        }
+
+	e.GET("/cats/:data", handler1)
+	
+	return e
+	
+	/*	
+  	//create groups
+	//adminGroup := e.Group("/admin")
 
 	//set all middlewares
-//	middlewares.SetMainMiddleWares(e)
-//	middlewares.SetAdminMiddlewares(adminGroup)
+	//middlewares.SetMainMiddleWares(e)
+	//middlewares.SetAdminMiddlewares(adminGroup)
 
 	//set main routes
-	api.MainGroup(e)
+	MainGroup(e)
 
 	//set groupRoutes
-//	api.AdminGroup(adminGroup)
-	return e
+	//api.AdminGroup(adminGroup)
+	e.GET("/health-check", handlers.HealthCheck)
+
+	e.GET("/cats/:data", handlers.GetCats)
+	e.POST("/cats", handlers.AddCat)
+	e.GET("/dogs", func(c echo.Context) error {       
+	return c.String(http.StatusOK, "Hello, Dogs!\n")  
+	})
+  	*/
 }
+
