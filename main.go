9598984dfb6f580cf0c643b6f8ccb82605b63fcb
allyite <89873811+allyite@users.noreply.github.com>
2021-09-03 17:04:43 +0530

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/allyite/goapi3/router"
+	"github.com/allyite/goapi3/helper"
 )
 
 func main() {
+	var mClient = helper.ConnectDB()
 	// create a new echo instance
-	e := router.New()
+	e := router.New(mClient)
 	e.Logger.Fatal(e.Start(":8000"))
 }
