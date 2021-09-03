1f415e10d8ba3cbe5e06d57573a81beac1c68183
allyite <89873811+allyite@users.noreply.github.com>
2021-09-03 17:21:16 +0530

    Update handler_stats.go

--- handlers/handler_stats.go
+++ handlers/handler_stats.go
@@ -22,37 +22,25 @@ func GetStats(c echo.Context, mclient *mongo.Client) error {
 	collection= helper.getMongoColl(mclient)
 	sName := c.QueryParam("name")
 	sName = sName[1:len(sName)-1]
-	dataType := c.Param("data")
-	if dataType == "string" {
-		var sr models.StateResp
-		filter := bson.M{"state": sName}
-		err := collection.FindOne(context.TODO(), filter).Decode(&sr)
-		if err != nil {
-			////helper.GetError(err, w)
-			return c.String(http.StatusOK, fmt.Sprintf("Error : %s\n", sName, err.Error()))
-		}
-		var sr2 models.StateResp
-		filter2 := bson.M{"state": "Total"}
-		err2 := collection.FindOne(context.TODO(), filter2).Decode(&sr2)
-		if err2 != nil {
-			////helper.GetError(err, w)
-			return c.String(http.StatusOK, fmt.Sprintf("Error : %s\nIndia", err.Error()))
-		}
-		////return c.JSON(http.StatusOK, sr)
-		out := "State: "+ sName + "\nActive Cases: " + sr.Active + "\nTotal Confirmed Cases: " + sr.Confirmed 
-		out = out + "\n\n" + "Country: India" + "\nActive Cases: " + sr2.Active + "\nTotal Confirmed Cases: " + sr2.Confirmed + "\nLast Updated Time: " + sr2.Lastupdatedtime
-		return c.String(http.StatusOK, fmt.Sprintf(out))
-		
-	} else if dataType == "json" {
-		state := models.State{
-			Name: sName,
-		}
-		return c.JSON(http.StatusOK, state)
-	} else {
-		return c.JSON(http.StatusBadRequest, map[string]string{
-			"error": "Please specify the data type as Sting or JSON",
-		})
+
+	var sr models.StateResp
+	filter := bson.M{"state": sName}
+	err := collection.FindOne(context.TODO(), filter).Decode(&sr)
+	if err != nil {
+		////helper.GetError(err, w)
+		return c.String(http.StatusOK, fmt.Sprintf("Error : %s\n", sName, err.Error()))
 	}
+	var sr2 models.StateResp
+	filter2 := bson.M{"state": "Total"}
+	err2 := collection.FindOne(context.TODO(), filter2).Decode(&sr2)
+	if err2 != nil {
+		////helper.GetError(err, w)
+		return c.String(http.StatusOK, fmt.Sprintf("Error : %s\nIndia", err.Error()))
+	}
+	////return c.JSON(http.StatusOK, sr)
+	out := "State: "+ sName + "\nActive Cases: " + sr.Active + "\nTotal Confirmed Cases: " + sr.Confirmed 
+	out = out + "\n\n" + "Country: India" + "\nActive Cases: " + sr2.Active + "\nTotal Confirmed Cases: " + sr2.Confirmed + "\nLast Updated Time: " + sr2.Lastupdatedtime
+	return c.String(http.StatusOK, fmt.Sprintf(out))
 
 }
 
