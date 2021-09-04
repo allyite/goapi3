d09dfc82bca325a76f3ccf4a1735b792240f2cc3
allyite <89873811+allyite@users.noreply.github.com>
2021-09-04 14:56:03 +0530

    Update handler_stats.go

--- handlers/handler_stats.go
+++ handlers/handler_stats.go
@@ -16,7 +16,7 @@ import (
 //	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-//http://localhost:8000/cats/json?name=arnold&type=fluffy
+
 //func GetStats(c echo.Context, collection *mongo.Collection) error {
 func GetStats(c echo.Context, mClient *mongo.Client) error {
 	collection := helper.GetMongoColl(mClient)
@@ -61,28 +61,3 @@ func GetStats(c echo.Context, mClient *mongo.Client) error {
 	//return c.String(http.StatusOK, fmt.Sprintf(out))
 
 }
-
-/*
-func getBook(w http.ResponseWriter, r *http.Request) {
-	// set header.
-	w.Header().Set("Content-Type", "application/json")
-
-	var book models.Book
-	// we get params with mux.
-	var params = mux.Vars(r)
-
-	// string to primitive.ObjectID
-	id, _ := primitive.ObjectIDFromHex(params["id"])
-
-	// We create filter. If it is unnecessary to sort data for you, you can use bson.M{}
-	filter := bson.M{"_id": id}
-	err := collection.FindOne(context.TODO(), filter).Decode(&book)
-
-	if err != nil {
-		helper.GetError(err, w)
-		return
-	}
-
-	json.NewEncoder(w).Encode(book)
-}
-*/
